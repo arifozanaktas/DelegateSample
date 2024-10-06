63e4812c6a397e751eeb5e48e54ac3b3290c3503
Arif Ozan AKTAŞ <aoaktas@hotmail.com>
2024-10-06 21:16:10 +0300

    Add project files.

---
+++ DelegateSample/MathExam.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DelegateSample;
+
+public delegate int MathExamDelegate(int x, int y);
+public class MathExam
+{
+    
+    public int MathExams(int a,int b, MathExamDelegate mathExamDelegate)
+    {
+        return mathExamDelegate(a,b);
+    }
+}
---
+++ DelegateSample/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace DelegateSample;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+
+        
+        var mathExam = new MathExam();
+        var result = mathExam.MathExams(10, 10, (a, b) => a * b + 100);
+        Console.WriteLine(result);
+
+
+    }
+}
